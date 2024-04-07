bb7e54a9377b2910a8b04a21c0031045acc2add6
RedTTY1 <161000428+RedTTY1@users.noreply.github.com>
2024-04-07 23:35:59 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,9 +20,8 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=15249640&pid=afaa99854200461684ec24c45b6aa6e4&tid=57653da5c7394f126d7923c9044b4dc1",
-    "https://www.maimemo.com/share/page?uid=16271827&pid=8410efff373f58836992dcba000643f8&tid=48fc91bc99020fd45e7475febe9628c5",
-    "https://www.maimemo.com/share/page?uid=10683082&pid=bbcbec87044c944539a708da3fff40c4&tid=c810a961e81f9195792a4bedf84e9b31",
+    
+    "https://www.maimemo.com/share/page?uid=10683082&pid=df4ac2dc16196455bcb23ea3503bf9ab&tid=68de38846dff281bf04fc20b01ae5b16",
   ]
   /**
    * 生成随机数字
